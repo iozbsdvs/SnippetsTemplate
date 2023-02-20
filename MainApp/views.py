b8007742d84e317ac046bd7a872b4208db58e1ba
iozbsdvs <Cayty@yandex.ru>
2023-02-20 22:08:51 +0300

    Project

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -9,8 +10,27 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET": #Хотим получить форму
+        form = SnippetForm()
+        context = {'pagename': 'Добавление нового сниппета',
+                   'form': form
+                   }
+        return render(request, 'pages/add_snippet.html', context)
+
+    if request.method == "POST": # Хотим создать Снипет (данные от формы)
+        # name = request.POST["name"]
+        # lang = request.POST["lang"]
+        # code = request.POST["code"]
+        # snippet = Snippet(name=name, lang=lang, code=code)
+        # snippet.save()
+        #
+        # return redirect('snippets-list')
+
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('snippets-list')
+
 
 
 def snippets_page(request):
