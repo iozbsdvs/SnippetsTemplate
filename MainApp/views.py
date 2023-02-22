6dc81d24845090429792847babf9f7878f1df457
iozbsdvs <Cayty@yandex.ru>
2023-02-22 09:26:45 +0300

    added authorization

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.contrib import auth
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
 
@@ -28,7 +29,9 @@ def add_snippet_page(request):
 
         form = SnippetForm(request.POST)
         if form.is_valid():
-            form.save()
+            snippet = form.save(commit=False)
+            snippet.user = request.user
+            snippet.save()
             return redirect('snippets-list')
 
 
@@ -54,3 +57,32 @@ def snippet_delete(request, snippet_id):
     snippet = Snippet.objects.get(id=snippet_id)
     snippet.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        # print("username =", username)
+        # print("password =", password)
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        else:
+            # Return error message
+            pass
+    return redirect('home')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+def snippets_user(request):
+    user = Snippet.objects.filter(user__username='admin')
+
+    context = {'pagename': 'Просмотр сниппетов',
+               'user': user}
+
+    return render(request, 'pages/view-snippets.html', context)
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
                   path('snippets/add', views.add_snippet_page, name="snippets-add"),
                   path('snippet/<int:snippet_id>/delete', views.snippet_delete, name="snippet-delete"),
                   path('snippets/list', views.snippets_page, name="snippets-list"),
+                  path('snippets/list_user', views.snippets_user, name="snippets-user"),
                   path('snippet/<int:snippet_id>', views.snippet_detail, name="snippet-detail"),
+                  path('login', views.login, name="login"),
+                  path('logout', views.logout, name="logout"),
+
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
